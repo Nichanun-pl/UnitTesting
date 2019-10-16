275b643ca066fed7a40a317fad9d75db31f28670
nichanun-pl <nichanun.pl@rmuti.ac.th>
2019-10-17 04:35:19 +0700

    2. Testing Strings

--- TestNinja.UnitTests/HtmlFormatterTests.cs
+++ TestNinja.UnitTests/HtmlFormatterTests.cs
@@ -1,8 +1,15 @@
-﻿
+﻿using NUnit.Framework;
+using TestNinja.Fundamentals;
+
 namespace TestNinja.UnitTests
 {
-    class HtmlFormatterTests
+    [TestFixture]
+    public class HtmlFormatterTests
     {
+        [Test]
+        public void FormatAsBold_WhenCalled_ShouldEncloseTheStringWithStrongElement()
+        {
 
+        }
     }
 }
