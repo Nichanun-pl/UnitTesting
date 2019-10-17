e77ab0e7876e587909f4b2e28654632ccfecb19e
nichanun-pl <nichanun.pl@rmuti.ac.th>
2019-10-18 02:20:11 +0700

    6. Solution- Stack

--- TestNinja.UnitTests/StackTests.cs
+++ TestNinja.UnitTests/StackTests.cs
@@ -1,9 +1,27 @@
 ﻿using NUnit.Framework;
+using TestNinja.Fundamentals;
 
 namespace TestNinja.UnitTests
 {
     [TestFixture]
     public class StackTests
     {
+        [Test]
+        public void Push_ArgIsNull_ThrowArgNullException()
+        {
+            var stack = new Stack<string>();
+
+            Assert.That(() => stack.Push(null), Throws.ArgumentNullException);
+        }
+
+        [Test]
+        public void Push_ValidArg_AddTheObjectToTheStack()
+        {
+            var stack = new Stack<string>();
+            
+            stack.Push("a");
+            
+            Assert.That(stack.Count, Is.EqualTo(1));
+        }
     }
 }
