e746e2abb543d8d56ff8c29a71de09fc412f61b8
nichanun-pl <nichanun.pl@rmuti.ac.th>
2019-10-18 04:00:12 +0700

    4. Dependency Injection via Method Parameters

--- TestNinja/Mocking/Program.cs
+++ TestNinja/Mocking/Program.cs
@@ -1,12 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TestNinja.Mocking
+﻿namespace TestNinja.Mocking
 {
-    class Program
+    public class Program
     {
+        public static void Main()
+        {
+            var service = new VideoService();
+            var title = service.ReadVideoTitle(new FileReader());
+        }
     }
 }
