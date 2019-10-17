4a41f1c33f5adfcf638dcba3d590214bdcce231f
nichanun-pl <nichanun.pl@rmuti.ac.th>
2019-10-18 00:58:18 +0700

    4. Solution- DemeritPointsCalculator

--- TestNinja.UnitTests/DemeritPointsCalculatorTests.cs
+++ TestNinja.UnitTests/DemeritPointsCalculatorTests.cs
@@ -1,4 +1,6 @@
 ﻿using NUnit.Framework;
+using System;
+using TestNinja.Fundamentals;
 
 namespace TestNinja.UnitTests
 {
@@ -6,15 +8,28 @@ namespace TestNinja.UnitTests
     public class DemeritPointsCalculatorTests
     {
         [Test]
-        public void CalculateDemeritPoints_SpeedIsNegative_ThrowArgumentOutOfRangeException()
+        [TestCase(-1)]
+        [TestCase(301)]
+        public void CalculateDemeritPoints_SpeedIsOutOfRange_ThrowArgumentOutOfRangeException(int speed)
+        {
+            var calculator = new DemeritPointsCalculator();
+            
+            Assert.That(() => calculator.CalculateDemeritPoints(speed),
+                Throws.Exception.TypeOf<ArgumentOutOfRangeException>());
+        }
+
+        [Test]
+        public void CalculateDemeritPoints_SpeedIsOver300_ThrowArgumentOutOfRangeException()
         {
         }
 
         [Test]
-        [TestCase(0, 0)]
+        [TestCase( 0, 0)]
         [TestCase(64, 0)]
         [TestCase(65, 0)]
-        [TestCase(66, 0)]
+        [TestCase(65, 0)]
+        [TestCase(70, 1)]
+        [TestCase(75, 2)]
         public void CalculateDemeritPoints_WhenCalled_ReturnDemeritPoints(int speed)
         {
         }
