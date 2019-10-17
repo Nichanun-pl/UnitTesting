7f91ff5d4cf80d9b8d24fa983c14bd436c1d7a5e
nichanun-pl <nichanun.pl@rmuti.ac.th>
2019-10-17 23:44:45 +0700

    4. Solution- DemeritPointsCalculator

--- TestNinja.UnitTests/DemeritPointsCalculatorTests.cs
+++ TestNinja.UnitTests/DemeritPointsCalculatorTests.cs
@@ -5,5 +5,19 @@ namespace TestNinja.UnitTests
     [TestFixture]
     public class DemeritPointsCalculatorTests
     {
+        [Test]
+        public void CalculateDemeritPoints_SpeedIsNegative_ThrowArgumentOutOfRangeException()
+        {
+        }
+
+        [Test]
+        public void CalculateDemeritPoints_SpeedIsLessThanOrEqualToSpeedLimit_ReturnZero(int speed)
+        {
+        }
+
+        [Test]
+        public void CalculateDemeritPoints_SpeedIsLessThanSpeedLimit_ReturnZero()
+        {
+        }
     }
 }
