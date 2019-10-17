34f1f827adc20b34f05cfa43140d98a35a79e7f2
nichanun-pl <nichanun.pl@rmuti.ac.th>
2019-10-18 01:01:39 +0700

    Revert "4. Solution- DemeritPointsCalculator"
    This reverts commit 4a41f1c33f5adfcf638dcba3d590214bdcce231f.

--- TestNinja.UnitTests/DemeritPointsCalculatorTests.cs
+++ TestNinja.UnitTests/DemeritPointsCalculatorTests.cs
@@ -1,6 +1,4 @@
 ﻿using NUnit.Framework;
-using System;
-using TestNinja.Fundamentals;
 
 namespace TestNinja.UnitTests
 {
@@ -8,28 +6,15 @@ namespace TestNinja.UnitTests
     public class DemeritPointsCalculatorTests
     {
         [Test]
-        [TestCase(-1)]
-        [TestCase(301)]
-        public void CalculateDemeritPoints_SpeedIsOutOfRange_ThrowArgumentOutOfRangeException(int speed)
-        {
-            var calculator = new DemeritPointsCalculator();
-            
-            Assert.That(() => calculator.CalculateDemeritPoints(speed),
-                Throws.Exception.TypeOf<ArgumentOutOfRangeException>());
-        }
-
-        [Test]
-        public void CalculateDemeritPoints_SpeedIsOver300_ThrowArgumentOutOfRangeException()
+        public void CalculateDemeritPoints_SpeedIsNegative_ThrowArgumentOutOfRangeException()
         {
         }
 
         [Test]
-        [TestCase( 0, 0)]
+        [TestCase(0, 0)]
         [TestCase(64, 0)]
         [TestCase(65, 0)]
-        [TestCase(65, 0)]
-        [TestCase(70, 1)]
-        [TestCase(75, 2)]
+        [TestCase(66, 0)]
         public void CalculateDemeritPoints_WhenCalled_ReturnDemeritPoints(int speed)
         {
         }
