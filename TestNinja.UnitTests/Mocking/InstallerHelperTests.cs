2836facce3d02225203d9aa3f7f813458e697f99
nichanun-pl <nichanun.pl@rmuti.ac.th>
2019-10-19 05:05:03 +0700

    6. Testing InstallerHelper

--- TestNinja.UnitTests/Mocking/InstallerHelperTests.cs
+++ TestNinja.UnitTests/Mocking/InstallerHelperTests.cs
@@ -1,5 +1,6 @@
 ﻿using Moq;
 using NUnit.Framework;
+using System.Net;
 using TestNinja.Mocking;
 
 namespace TestNinja.UnitTests.Mocking
@@ -15,6 +16,16 @@ namespace TestNinja.UnitTests.Mocking
         {
             _fileDownloader = new Mock<IFileDownloader>();
             _installerHelper = new InstallerHelper(_fileDownloader.Object);
+        }
+
+        [Test]
+        public void DownloaderInstaller_DownloaderFile_ReturnFalse()
+        {
+            _fileDownloader.Setup(fd => fd.DownloadFile("", "")).Throws<WebException>();
+
+            _installerHelper.DownloadInstaller("customer", "installer");
+
+            Assert.That(result, Is.False);
         }
     }
 }
