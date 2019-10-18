7c9f1702c8cfaf01be7cd120f081b40bcdd033fa
nichanun-pl <nichanun.pl@rmuti.ac.th>
2019-10-19 03:33:35 +0700

    6-3. Testing

--- TestNinja.UnitTests/Mocking/VideoServiceTests.cs
+++ TestNinja.UnitTests/Mocking/VideoServiceTests.cs
@@ -8,12 +8,14 @@ namespace TestNinja.UnitTests.Mocking
     public class VideoServiceTests
     {
         private Mock<IFileReader> _fileReader;
+        private Mock<IVideoRepository> _repository;
         private VideoService _videoService;
 
         [SetUp]
         public void SetUp()
         {
             _fileReader = new Mock<IFileReader>();
+            _repository = new Mock<IVideoRepository>();
             _videoService = new VideoService(_fileReader.Object);
 
         }
@@ -26,6 +28,12 @@ namespace TestNinja.UnitTests.Mocking
             var result = _videoService.ReadVideoTitle();
 
             Assert.That(result, Does.Contain("error").IgnoreCase);
+        }
+
+        [Test]
+        public void GetUnprocessedVideosAsCsv_AllVideosAreProcessed_ReturnAnEeptyString()
+        {
+
         }
     }
 }
