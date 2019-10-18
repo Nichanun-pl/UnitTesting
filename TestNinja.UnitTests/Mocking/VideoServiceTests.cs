e11632463047d5f87d4420dac667bee4db26a5bd
nichanun-pl <nichanun.pl@rmuti.ac.th>
2019-10-19 00:32:11 +0700

    Revert "5-9. Creating Mock Objects Using Moq"
    This reverts commit c60ad0a5b7c0794bcd12ea6f89c156a14ed4550d.

--- TestNinja.UnitTests/Mocking/VideoServiceTests.cs
+++ TestNinja.UnitTests/Mocking/VideoServiceTests.cs
@@ -1,6 +1,5 @@
 ﻿using NUnit.Framework;
 using TestNinja.Mocking;
-using Moq;
 
 namespace TestNinja.UnitTests.Mocking
 {
@@ -10,9 +9,6 @@ namespace TestNinja.UnitTests.Mocking
         [Test]
         public void ReadVideoTitle_EmptyFile_ReturnError()
         {
-            var mockFileReader = new Mock<IFileReader>();
-
-
             var service = new VideoService(new FakeFileReader());
 
             var result = service.ReadVideoTitle();
