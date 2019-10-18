cd30e5371a36667371b6714e0392fa26e39b68fd
nichanun-pl <nichanun.pl@rmuti.ac.th>
2019-10-19 01:33:44 +0700

    9. Creating Mock Objects Using Moq

--- TestNinja.UnitTests/Mocking/VideoServiceTests.cs
+++ TestNinja.UnitTests/Mocking/VideoServiceTests.cs
@@ -7,16 +7,22 @@ namespace TestNinja.UnitTests.Mocking
     [TestFixture]
     public class VideoServiceTests
     {
-        [Test]
-        public void ReadVideoTitle_EmptyFile_ReturnError()
+        private Mock<IFileReader> _fileReader;
+        private VideoService _videoService;
+
+        [SetUp]
+        public void SetUp()
         {
             _fileReader = new Mock<IFileReader>();
             _videoService = new VideoService(_fileReader.Object);
 
+        }
+
+        [Test]
+        public void ReadVideoTitle_EmptyFile_ReturnError()
+        {
             _fileReader.Setup(fr => fr.Read("video.txt")).Returns("");
-
             
-
             var result = _videoService.ReadVideoTitle();
 
             Assert.That(result, Does.Contain("error").IgnoreCase);
