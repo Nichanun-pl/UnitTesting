297617af2cc09a7880fb781784bb6ca3099c52f3
nichanun-pl <nichanun.pl@rmuti.ac.th>
2019-10-19 07:56:57 +0700

    4. Writing the First Test

--- TestNinja.UnitTests/Mocking/BookingHelperTests.cs
+++ TestNinja.UnitTests/Mocking/BookingHelperTests.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using Moq;
 using TestNinja.Mocking;
 using System.Collections.Generic;
+using System;
 
 namespace TestNinja.UnitTests.Mocking
 {
@@ -15,6 +16,13 @@ namespace TestNinja.UnitTests.Mocking
             var mock = new Mock<IBookingRepository>();
             mock.Setup(r => r.GetActiveBookings(1)).Returns(new List<Booking>
             {
+                new Booking
+                {
+                    Id = 2,
+                    ArrivalDate = new DateTime(2017, 1, 15, 14, 0, 0),
+                    DepartureDate = new DateTime(2017, 1, 20, 10, 0, 0),
+                    Reference = "a"
+                }
 
             }.AsQueryable());
         }
