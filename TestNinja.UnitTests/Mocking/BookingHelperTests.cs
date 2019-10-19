889454d11a17569c32b406570633327bc0d6fe79
nichanun-pl <nichanun.pl@rmuti.ac.th>
2019-10-19 08:09:10 +0700

    4. Writing the First Test

--- TestNinja.UnitTests/Mocking/BookingHelperTests.cs
+++ TestNinja.UnitTests/Mocking/BookingHelperTests.cs
@@ -26,12 +26,14 @@ namespace TestNinja.UnitTests.Mocking
 
             }.AsQueryable());
 
-            BookingHelper.OverlappingBookingsExist(new Booking
+            var result = BookingHelper.OverlappingBookingsExist(new Booking
             {
                 Id = 1,
-                ArrivalDate = new DateTime(2017, 1, 15, 14, 0, 0),
-                DepartureDate = new DateTime(2017, 1, 20, 10, 0, 0),
+                ArrivalDate = new DateTime(2017, 1, 10, 14, 0, 0),
+                DepartureDate = new DateTime(2017, 1, 24, 10, 0, 0),
             }, repository.Object);
+
+            Assert.That(result, Is.Empty);
         }
     }
 }
