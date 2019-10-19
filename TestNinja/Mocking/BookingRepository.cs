1ca2ae2c03c56226c62ef768bf9b22ab00c574d8
nichanun-pl <nichanun.pl@rmuti.ac.th>
2019-10-19 07:11:28 +0700

    3. Extracting IBooking Repository

--- TestNinja/Mocking/BookingRepository.cs
+++ TestNinja/Mocking/BookingRepository.cs
@@ -1,6 +1,18 @@
-﻿namespace TestNinja.Mocking
+﻿using System.Linq;
+
+namespace TestNinja.Mocking
 {
     public class BookingRepository
     {
+        public IQueryable<Booking> GetActiveBookings()
+        {
+            var unitOfWork = new UnitOfWork();
+            var bookings =
+                unitOfWork.Query<Booking>()
+                    .Where(
+                        b => b.Id != booking.Id && b.Status != "Cancelled");
+
+            return bookings;
+        }
     }
 }
