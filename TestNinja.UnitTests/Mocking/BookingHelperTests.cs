bd24dae39d1ef51a4e1fdf56bf699ca310c2baa2
nichanun-pl <nichanun.pl@rmuti.ac.th>
2019-10-19 08:00:38 +0700

    4. Writing the First Test

--- TestNinja.UnitTests/Mocking/BookingHelperTests.cs
+++ TestNinja.UnitTests/Mocking/BookingHelperTests.cs
@@ -13,8 +13,8 @@ namespace TestNinja.UnitTests.Mocking
         [Test]
         public void BookingStartsAndFinishesBeforeAnExistingBooking_ReturnEmptyString()
         {
-            var mock = new Mock<IBookingRepository>();
-            mock.Setup(r => r.GetActiveBookings(1)).Returns(new List<Booking>
+            var repository = new Mock<IBookingRepository>();
+            repository.Setup(r => r.GetActiveBookings(1)).Returns(new List<Booking>
             {
                 new Booking
                 {
@@ -25,6 +25,13 @@ namespace TestNinja.UnitTests.Mocking
                 }
 
             }.AsQueryable());
+
+            BookingHelper.OverlappingBookingsExist(new Booking
+            {
+                Id = 1,
+                ArrivalDate = new DateTime(2017, 1, 15, 14, 0, 0),
+                DepartureDate = new DateTime(2017, 1, 20, 10, 0, 0),
+            }, repository.Object);
         }
     }
 }
