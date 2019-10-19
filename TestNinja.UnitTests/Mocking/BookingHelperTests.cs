5db46a96cc957c50592f18afd111ef47cf063f2a
nichanun-pl <nichanun.pl@rmuti.ac.th>
2019-10-19 09:12:05 +0700

    7-7. Fixing a Bug

--- TestNinja.UnitTests/Mocking/BookingHelperTests.cs
+++ TestNinja.UnitTests/Mocking/BookingHelperTests.cs
@@ -60,7 +60,21 @@ namespace TestNinja.UnitTests.Mocking
             Assert.That(result, Is.EqualTo(_existingBooking.Reference));
         }
 
-            private DateTime Before(DateTime dateTime, int days = 1)
+        [Test]
+        public void BookingStartsBeforeAndFinishesAfterOfAnExistingBooking_ReturnExistingBookingsReference()
+        {
+
+            var result = BookingHelper.OverlappingBookingsExist(new Booking
+            {
+                Id = 1,
+                ArrivalDate = Before(_existingBooking.ArrivalDate),
+                DepartureDate = After(_existingBooking.DepartureDate)
+            }, _repository.Object);
+
+            Assert.That(result, Is.EqualTo(_existingBooking.Reference));
+        }
+
+        private DateTime Before(DateTime dateTime, int days = 1)
         {
             return dateTime.AddDays(-days);
         }
