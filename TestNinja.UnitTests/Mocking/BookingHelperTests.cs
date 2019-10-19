2147fb8a3293a750e6cd8096e879560d83fd27ab
nichanun-pl <nichanun.pl@rmuti.ac.th>
2019-10-19 09:36:54 +0700

    8. Writing Additional Tests

--- TestNinja.UnitTests/Mocking/BookingHelperTests.cs
+++ TestNinja.UnitTests/Mocking/BookingHelperTests.cs
@@ -115,6 +115,21 @@ namespace TestNinja.UnitTests.Mocking
             Assert.That(result, Is.Empty);
         }
 
+        [Test]
+        public void BookingOverlapButNewBookingIsCancelled_ReturnExistingBookingsReference()
+        {
+
+            var result = BookingHelper.OverlappingBookingsExist(new Booking
+            {
+                Id = 1,
+                ArrivalDate = After(_existingBooking.ArrivalDate),
+                DepartureDate = After(_existingBooking.DepartureDate),
+                Status = "Cancelled"
+            }, _repository.Object);
+
+            Assert.That(result, Is.Empty);
+        }
+
         private DateTime Before(DateTime dateTime, int days = 1)
         {
             return dateTime.AddDays(-days);
