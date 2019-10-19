79edd78e1e19268aaba41c3b4d64b295f9da2d15
nichanun-pl <nichanun.pl@rmuti.ac.th>
2019-10-19 23:53:18 +0700

    10. Testing Exceptions

--- TestNinja.UnitTests/Mocking/HouseKeeperServiceTests.cs
+++ TestNinja.UnitTests/Mocking/HouseKeeperServiceTests.cs
@@ -131,11 +131,16 @@ namespace TestNinja.UnitTests.Mocking
         [Test]
         public void SendStatementEmails_EmailSendingFails_DisplayAMessageBox()
         {
-            _statementFileName = " ";
+            _emailSender.Setup(es => es.EmailFile(
+                It.IsAny<string>(),
+                It.IsAny<string>(),
+                It.IsAny<string>(),
+                It.IsAny<string>()
+            )).Throws<Exception>();
 
             _service.SendStatementEmails(_statementDate);
 
-            VerifyEmailNotSent();
+            _messageBox.Verify(mb => mb.Show(It.IsAny<string>(), It.IsAny<string>(), MessageBoxButtons.OK));
         }
 
         private void VerifyEmailNotSent()
