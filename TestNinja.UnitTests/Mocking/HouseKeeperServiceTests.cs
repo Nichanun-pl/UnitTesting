57b27872da9f3dbac8530f80645e09d1b5029676
nichanun-pl <nichanun.pl@rmuti.ac.th>
2019-10-19 20:48:48 +0700

    5. Writing the First Interaction Test

--- TestNinja.UnitTests/Mocking/HouseKeeperServiceTests.cs
+++ TestNinja.UnitTests/Mocking/HouseKeeperServiceTests.cs
@@ -1,6 +1,22 @@
-﻿namespace TestNinja.UnitTests.Mocking
+﻿using System;
+using System.Collections.Generic;
+using Moq;
+using NUnit.Framework;
+using TestNinja.Mocking;
+
+namespace TestNinja.UnitTests.Mocking
 {
+    [TestFixture]
     public class HouseKeeperServiceTests
     {
+        [Test]
+        public void SendStatementEmails_WhenCalled_GenerateStatements()
+        {
+            var unitOfWork = new Mock<IUnitOfWork>();
+            unitOfWork.Setup(uow => uow.Query<Housekeeper>()).Returns(new List<Housekeeper> 
+            { 
+
+            })
+        }
     }
 }
